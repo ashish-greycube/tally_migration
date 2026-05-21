e869e39f200d384d9b0947089c4be6261d807cb5
jigar-greycube <jigar@greycube.in>
2026-05-21 16:49:20 +0530

    feat:Consider All Records For JE

--- tally_migration/tally_migration/doctype/tally_migration/tally_migration.py
+++ tally_migration/tally_migration/doctype/tally_migration/tally_migration.py
@@ -599,12 +599,15 @@ class TallyMigration(Document):
 
 	def _import_day_book_data(self):
 		def create_fiscal_years(vouchers):
+			print(vouchers)
 			from frappe.utils.data import add_years, getdate
 
 			earliest_date = getdate(min(voucher["posting_date"] for voucher in vouchers))
+			print("=================\n", earliest_date)
 			oldest_year = frappe.get_all(
 				"Fiscal Year", fields=["year_start_date", "year_end_date"], order_by="year_start_date"
 			)[0]
+			print("=========================\n",oldest_year)
 			while earliest_date < oldest_year.year_start_date:
 				new_year = frappe.get_doc({"doctype": "Fiscal Year"})
 				new_year.year_start_date = add_years(oldest_year.year_start_date, -1)
@@ -684,7 +687,7 @@ class TallyMigration(Document):
 					"_import_vouchers",
 					queue="long",
 					timeout=3600,
-					start=index + 1,
+					start=index,
 					total=total,
 					is_last=is_last,
 				)
@@ -738,7 +741,8 @@ class TallyMigration(Document):
 	@frappe.whitelist()
 	def import_day_book_data(self):
 		self.set_status("Importing Day Book Data")
-		frappe.enqueue_doc(self.doctype, self.name, "_import_day_book_data", queue="long", timeout=3600)
+		# frappe.enqueue_doc(self.doctype, self.name, "_import_day_book_data", queue="long", timeout=3600)
+		self._import_day_book_data()
 
 	def log(self, data=None):
 		if isinstance(data, frappe.model.document.Document):
